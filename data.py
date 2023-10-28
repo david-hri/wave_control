10ac5b2306ece597877e787734afde671aeee33b
2022hourida <david.houri@student-cs.fr>
2023-10-28 12:01:04 +0200

    init

---
+++ data.py
@@ -0,0 +1,54 @@
+import numpy as np
+from math import *
+L=10
+sigma=14000  
+phi=0.99
+rho0=1.29
+alpha_h=1.02
+gamma_p=7/5
+c0=340
+Ksi0=1/c0/c0
+Ksi1=Ksi0*gamma_p*phi
+a0=0
+a1=sigma*phi**2*gamma_p/(c0**2*rho0*alpha_h)
+eta0=1
+eta1=phi/alpha_h
+
+def f_lambda0(k, omega):
+    try:
+        if k**2 >= Ksi0 * eta0 * omega**2:
+            return np.sqrt(k**2 - Ksi0 / eta0 * omega**2)
+        else:
+            return 1j * np.sqrt(Ksi0 /eta0 * omega**2 - k**2)
+    except:
+        print(k,omega,"erreur")
+    
+
+def f_lambda1(k,w):
+    k2=k**2 #holder
+    w2=Ksi1/eta1*w**2
+    diff=k2-w2
+    var1=(a1/eta1*w)**2 #holder
+    var2=sqrt(diff**2+var1)
+    real=sqrt(diff+var2)
+    real=1/sqrt(2)*real
+    im=sqrt(-diff+var2)
+    im=-1/sqrt(2)*im
+    return complex(real,im)
+
+
+# Fonction f(x)
+def f( lambda0,x):
+    return (lambda0 * eta0 - x) * np.exp(-lambda0 * L) + (lambda0 * eta0 + x) * np.exp(lambda0 * L)
+
+# Fonction χ(k, α)
+def f_chi(k, lambda0, lambda1, gk,alpha):
+    result = gk * ((lambda0 * eta0 - lambda1 * eta1)/f(lambda0,lambda1*eta1)- (lambda0 * eta0 - alpha)/f(lambda0,alpha))
+    return result
+
+# Fonction γ(k, α)
+def f_gamma(k, alpha, lambda0,lambda1, gk):
+    numerator = gk * ((lambda0 * eta0 + lambda1 * eta1)/f(lambda0,lambda1*eta1)- (lambda0 * eta0 + alpha)/f(lambda0,alpha))
+    return numerator
+
+
---
+++ functions.py
@@ -0,0 +1,85 @@
+
+from scipy import signal
+from math import *
+from data import *
+import cmath
+from scipy.integrate import quad
+
+from scipy.optimize import minimize_scalar,minimize
+
+
+
+# on notera w pour omega
+def g1(y,w,amplitude=1.0):  # gausienne pour modéliser un bruit localiser
+    try:
+        if -L <= y <= L:
+            return amplitude * sin(w * y)
+        else:
+            raise ValueError("Error: y is outside the valid range [-L, L]")
+    except ValueError as e:
+        return str(e)
+
+
+def g2(y,w, amplitude=1.0): # 1 si w in [20,20000], pour modéliser une salle de classe
+    try:
+        if -L<=y<=L:
+            freq=w/(2*pi)
+            if 20<=freq<=20000:
+                return 1    
+            else: return 0
+        else: 
+            raise ValueError("Error: y is outside the valid range [-L, L]")
+    except ValueError as e:
+        return str(e)
+
+def g3(y,w, amplitude =1.0,w0=1000,sigma=1,k_sirène=0.1): #sirène , sous la forme d'un signal carr;e
+     #la sirène se trouve entre -k_sirène*L et k_sirène*L
+    if -k_sirène*L<=y<=k_sirène*L:
+        return amplitude *exp(-(w-w0)**2/sigma)
+    else:
+        return 0
+    
+
+def fourrier_k(g,w,k): #calcul le coefficient k de la transformée de fourrier de g 
+    def gprime(y):
+        return g(y,w) * np.exp(-complex(0,1) * np.pi * k * y /L) #le calcul du coefficient de fourrier nous fait calculer l'intégrale de cette fonction entre -L et L
+    result, error = quad(gprime, -L, L)
+    return result
+
+
+
+
+def calculate_e_k(alpha, w, k,g,A=1,B=1):
+    lambda0=f_lambda0(k,w)
+    lambda1=f_lambda1(k,w)
+    gk=fourrier_k(g,w,k)
+    chi=f_chi(k, lambda0, lambda1, gk,alpha)
+    gamma=f_gamma(k, alpha, lambda0,lambda1, gk)
+    
+    if k**2 >= Ksi0 / eta0 * w**2:
+        result = (A + B * k**2) * (L * (abs(chi)**2 + abs(gamma)**2) +
+                 (1j / lambda0) * (chi.conjugate() * gamma * (1 - cmath.exp(-2 * lambda0 * L))))
+        result += B * L * abs(lambda0)**2 * (abs(chi)**2 + abs(gamma)**2) + 1j * B * lambda0 * (chi.conjugate() * gamma * (1 - cmath.exp(-2 * lambda0 * L)))
+    else:
+        result = (A + B * k**2) * (1 / (2 * lambda0) * ((abs(chi)**2 * (1 - cmath.exp(-2 * lambda0 * L))) + (abs(gamma)**2 * (cmath.exp(2 * lambda0 * L) - 1))) + 2 * L * (chi.real * gamma.real))
+        result += B * (lambda0 / 2) * ((abs(chi)**2 * (1 - cmath.exp(-2 * lambda0 * L)) + abs(gamma)**2 * (cmath.exp(2 * lambda0 * L) - 1)) - 2 * B * lambda0**2 * L * (chi.real * gamma.real))
+    return result
+
+
+def e(alpha,w,N,g):
+    result=0
+    for i in range(-N,N+1):
+        k=i*np.pi/L
+        result+=calculate_e_k(alpha,w,k,g)
+    return result
+
+
+def optimal_alpha(w,N,g):
+    init=1+1j
+    def optimize_e(alpha):
+        return abs(e(alpha,w,N,g))
+    result = minimize(optimize_e,init)
+    return result
+
+
+print(optimal_alpha(1000,5,g2))
